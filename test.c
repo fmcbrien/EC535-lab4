e9dc02497e8146bd4e9f5cca2561db0622f7cb7b
Fallon McBrien <56775119+fmcbrien@users.noreply.github.com>
2025-10-24 12:42:04 -0400

    rev 3 red light test

--- test.c
+++ test.c
@@ -17,26 +17,16 @@ static int mytraffic_init(void)
 
 
   // REQUEST GPIO FOR LEDs
-  gpio_request(RED_LED_GPIO, "RED_LED");
-  gpio_request(YELLOW_LED_GPIO, "YELLOW_LED");
-  gpio_request(GREEN_LED_GPIO, "GREEN_LED");
+  gpio_request(RED_LED_GPIO, "GPIO_67");
 
   // SET DIRECTION FOR LEDs
   gpio_direction_output(RED_LED_GPIO, 1);
-  gpio_direction_output(YELLOW_LED_GPIO, 0);
-  gpio_direction_output(GREEN_LED_GPIO, 0);
 
-
-  // REQUEST GPIO FOR BUTTONS
-  gpio_request(BUTTON0_GPIO, "BTN0");
-  gpio_request(BUTTON1_GPIO, "BTN1");
-
-  // SET DIRECTION FOR BUTTONS
-  gpio_direction_input(BUTTON0_GPIO);
-  gpio_direction_input(BUTTON1_GPIO);
+  // SET VALUE FOR LEDs
+  gpio_set_value(RED_LED_GPIO, 1);
    
-    printk(KERN_INFO "mytraffic module loaded\n");
-    return 0;
+  printk(KERN_INFO "mytraffic module loaded\n");
+  return 0;
 }
 
 module_init(mytraffic_init);
