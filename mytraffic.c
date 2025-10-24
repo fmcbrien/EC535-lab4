b91aebaea4181e2bf21392b01472a8c1ad634eb8
Fallon McBrien <56775119+fmcbrien@users.noreply.github.com>
2025-10-24 15:17:37 -0400

    Normal pattern and flashing red pattern

--- mytraffic.c
+++ mytraffic.c
@@ -5,9 +5,18 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 
+#define DEVICE_NAME "mytraffic"
+#define DEVICE_MAJOR 61
+#define DEVICE_MINOR 0
+
 #define RED_LED_GPIO 67  // GPIO PIN FOR RED LED
 #define YELLOW_LED_GPIO 68 // GPIO PIN FOR YELLOW LED
 #define GREEN_LED_GPIO 44 // GPIO PIN FOR GREEN LED
+
+#define BTN0_GPIO 26    // GPIO PIN FOR BUTTON 0
+#define BTN1_GPIO 46    // GPIO PIN FOR BUTTON 1
+
+#define CYCLE_TIME 1    // CYCLE TIME FOR TRAFFIC CYCLES
 
 int set_output(unsigned int pin){
   int ret;
@@ -24,41 +33,67 @@ static int __init mytraffic_init(void)
   int ret;
 
   printk(KERN_INFO "mytraffic module loaded\n");
+
+  // FLAGS
+  int mode = 0; // 0 = normal, 1 = red-flashing, 2 = yellow-flashing
   
   // REQUEST GPIO FOR LEDs
   ret = gpio_request(RED_LED_GPIO, "GPIO_67");
-  if (ret) {
-     printk(KERN_ERR "Failed to request GPIO %d\n", RED_LED_GPIO);
-     return ret;
-   }
   ret = gpio_request(YELLOW_LED_GPIO, "GPIO_68");
-  if (ret) {
-     printk(KERN_ERR "Failed to request GPIO %d\n", YELLOW_LED_GPIO);
-     return ret;
-   }
   ret = gpio_request(GREEN_LED_GPIO, "GPIO_44");
-  if (ret) {
-     printk(KERN_ERR "Failed to request GPIO %d\n", GREEN_LED_GPIO);
-     return ret;
-   }
+  ret = gpio_request(BTN0_GPIO, "GPIO_26");
+  ret = gpio_request(BTN1_GPIO, "GPIO_46");
 
-  // SET DIRECTION FOR LEDs
+  // SET DIRECTION FOR LEDs AND BUTTONS
   ret = set_output(RED_LED_GPIO);
   ret = set_output(YELLOW_LED_GPIO);
   ret = set_output(GREEN_LED_GPIO);
+  // TODO set buttons up
+    int i;
 
-  // SET VALUE FOR LEDs
-  gpio_set_value(RED_LED_GPIO, 1);
-  gpio_set_value(YELLOW_LED_GPIO, 1);
-  gpio_set_value(GREEN_LED_GPIO, 1);
   
-   
+  // Normal loop default
+  int count = 0; // count for which cycle we are on
+  for(i = 0; i < 21; i++) {
+    if (count == 0) {
+      gpio_set_value(RED_LED_GPIO, 0);
+      gpio_set_value(GREEN_LED_GPIO, 1);
+      count ++;
+    } else if (count == 3) {
+      gpio_set_value(GREEN_LED_GPIO, 0);
+      gpio_set_value(YELLOW_LED_GPIO, 1);
+      count ++;
+    } else if (count == 4) {
+      gpio_set_value(YELLOW_LED_GPIO, 0);
+      gpio_set_value(RED_LED_GPIO, 1);
+      count ++;
+    } else if (count == 5) {
+      count = 0;
+    } else {
+      count ++;
+    }
+    ssleep(CYCLE_TIME);
+  }
+
+  // Flashing-red
+  for(i = 0; i < 15; i++) {
+    gpio_set_value(RED_LED_GPIO, 0);
+    ssleep(CYCLE_TIME);
+    gpio_set_value(RED_LED_GPIO, 1);
+    ssleep(CYCLE_TIME);
+  }
+  
   return 0;
 }
 
-static void __exit mytraffic_exit(void)
-{
+static void __exit mytraffic_exit(void) {
+  gpio_set_value(RED_LED_GPIO, 0);
+  gpio_set_value(YELLOW_LED_GPIO, 0);
+  gpio_set_value(GREEN_LED_GPIO, 0);
+  
   gpio_free(RED_LED_GPIO);
+  gpio_free(YELLOW_LED_GPIO);
+  gpio_free(GREEN_LED_GPIO);
   printk(KERN_INFO "mytraffic module unloaded\n");
 }
 
