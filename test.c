352e62f2a51b579e529cb64cf1fea5b833ac03cf
Fallon McBrien <56775119+fmcbrien@users.noreply.github.com>
2025-10-24 12:31:33 -0400

    rev 2 red light on

--- test.c
+++ test.c
@@ -2,9 +2,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
-#include <linux/fs.h>
-#include <linux/interrupt.h>
-#include <linux/gpio/consumer.h>
 
 
 #define RED_LED_GPIO    67  // GPIO PIN FOR RED LED
@@ -15,36 +12,28 @@
 #define BUTTON0_GPIO   26  // GPIO PIN FOR BUTTON 0
 #define BUTTON1_GPIO   46  // GPIO PIN FOR BUTTON 1
 
-
-static struct gpio_desc *red_led;
-static struct gpio_desc *yellow_led;
-static struct gpio_desc *green_led;
-
-
-static struct gpio_desc *button0;
-static struct gpio_desc *button1;
-
-
 static int mytraffic_init(void)
 {
 
 
-    // REQUEST GPIO FOR LEDS
-    red_led = gpio_to_desc(RED_LED_GPIO);
-    yellow_led = gpio_to_desc(YELLOW_LED_GPIO);
-    green_led = gpio_to_desc(GREEN_LED_GPIO);
+  // REQUEST GPIO FOR LEDs
+  gpio_request(RED_LED_GPIO, "RED_LED");
+  gpio_request(YELLOW_LED_GPIO, "YELLOW_LED");
+  gpio_request(GREEN_LED_GPIO, "GREEN_LED");
 
-    gpiod_direction_output(red_led, 1);
-    gpiod_direction_output(yellow_led, 0);
-    gpiod_direction_output(green_led, 0);
+  // SET DIRECTION FOR LEDs
+  gpio_direction_output(RED_LED_GPIO, 1);
+  gpio_direction_output(YELLOW_LED_GPIO, 0);
+  gpio_direction_output(GREEN_LED_GPIO, 0);
 
 
-    // REQUEST GPIO FOR BUTTONS
-    button0 = gpio_to_desc(BUTTON0_GPIO);
-    button1 = gpio_to_desc(BUTTON1_GPIO);
+  // REQUEST GPIO FOR BUTTONS
+  gpio_request(BUTTON0_GPIO, "BTN0");
+  gpio_request(BUTTON1_GPIO, "BTN1");
 
-    gpiod_direction_input(button0);
-    gpiod_direction_input(button1);
+  // SET DIRECTION FOR BUTTONS
+  gpio_direction_input(BUTTON0_GPIO);
+  gpio_direction_input(BUTTON1_GPIO);
    
     printk(KERN_INFO "mytraffic module loaded\n");
     return 0;
